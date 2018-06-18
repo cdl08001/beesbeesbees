2d1aa08f135990eee1f6256270b7a6d1e409d335
Calin Lewis <calin.lewis@gmail.com>
2018-06-18 11:19:40 -0700

    Completed: HoneyMakerBee.js
    All tests passing

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,37 @@
-var HoneyMakerBee = function() {
+var Grub = function() {
+  this.age: 0,
+  this.color: 'pink',
+  this.food = 'jelly',
 };
 
+Grub.prototype.eat = function(){
+  
+}
+
+var Bee = function() {
+  Grub.call(this),
+  this.age = 5,
+  this.color = 'yellow',
+  this.job = 'keep on growing'
+};
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
+var HoneyMakerBee = function() {
+  Bee.call(this),
+  this.age = 10,
+  this.job = 'make honey',
+  this.color = 'yellow',
+  this.honeyPot = 0,
+
+};
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.honeyPot = function(){
+  this.honeyPot++;
+}
+HoneyMakerBee.prototype.giveHoney = function(){
+  this.honeyPot--;
+}
